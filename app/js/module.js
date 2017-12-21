81741139a591f72d8a483983b771e6c560b937fd
wxd <470911438@qq.com>
2017-12-21 09:31:18 +0800

    暂时能跳转，还有些不对，但是能跳了

--- app/js/module.js
+++ app/js/module.js
@@ -1,4 +1,4 @@
-angular.module('RDash', ['ui.bootstrap', 'ui.router', 'ngCookies','ngBaiduMap','ngDialog', 'smart-table'])
+angular.module('RDash', ['ui.router'])
     .constant('baseUrl','http://112.74.62.114:8080/Entity/Udb7fe87147e10/SZLKD')
     .constant('baiduUrl','http://api.map.baidu.com/geocoder/v2/?callback=renderReverse&output=json&ak=nhB87EF7jEp0diMOOTjHkXYyHQwGKojT')
     .constant('bmapUrl','http://api.map.baidu.com/staticimage/v2?ak=nhB87EF7jEp0diMOOTjHkXYyHQwGKojT');
